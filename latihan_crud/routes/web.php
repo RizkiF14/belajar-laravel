4d51aae921e1e859c2003eaec1e95edd217466a9
Rizki <rizki.furgana14@gmail.com>
2023-11-13 19:48:18 +0700

    Challenge1

---
+++ latihan_crud/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('students', [StudentController::class, 'index'])->name('students.index');
+Route::post('students', [StudentController::class, 'store'])->name('students.store');
+Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
